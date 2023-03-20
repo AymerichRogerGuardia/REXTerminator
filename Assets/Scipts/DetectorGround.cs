93bb0075b6a94c7a6bc85a2f3a5890ed04c1d485
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-20 15:13:35 +0100

    Mecanica de Saltar y casi fet el detector ground

--- Assets/Scipts/DetectorGround.cs
+++ Assets/Scipts/DetectorGround.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class DetectorGround : MonoBehaviour
 {
-    public bool grounded;
-   
-    private void OnTriggerStay2D(Collider2D collision)
+    [SerializeField] BoxCollider2D boxCollider2d;
+    [SerializeField] public LayerMask groundLayerMask;
+    Rigidbody2D rb;
+
+    public GameObject groundRayObect;
+    private void Update()
     {
-        grounded = true;
+        //Aqui crido la funcio de cuan
+        IsGrounded();
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D hitGround = Physics2D.Raycast (groundRayObect.transform.position, -Vector2.up, groundLayerMask);
+        Color rayColor;
+        if (hitGround.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+
+        /*
+        float HeightText = .01f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.down, boxCollider2d.bounds.extents.y + HeightText, groundLayerMask);
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(boxCollider2d.bounds.center, Vector2.down * (boxCollider2d.bounds.extents.y + HeightText) , rayColor);
+        Debug.Log(raycastHit.collider);
+        */
+        Debug.DrawRay(groundRayObect.transform.position, -Vector2.up * hitGround.distance);
+        Debug.Log(hitGround.collider);
+        return hitGround.collider != null;
+       
     }
 
 }
--- Assets/Scipts/Jump.cs
+++ Assets/Scipts/Jump.cs
@@ -2,24 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomJump : MonoBehaviour
+public class Jump : MonoBehaviour
 {
+    //declaro el objecte y li posu la variable i li indico un valor
     [SerializeField] float jumpForce = 7;
-
+    
+    //declaro el objecte del cos y li posu un nom perque entengui que es el cos
     Rigidbody2D rb;
 
-    // Start is called before the first frame update
     private void Start()
     {
+      //en el primer frame li indico que la palaula rb es igual al component de rigidbody que es el cos del objecte
       rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        //aqui faig que mentre la tecla estigui presionada lo que primer fara es pujar el objecte, despues multiplicara la força del objecte amb un valor , y per ultim lo que fa el ForceMode2D es canviar de força a impuls fent que l'objecte puji cap a d'alt
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            rb.AddForce(Vector2.up * jumpForce, );
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
     }
